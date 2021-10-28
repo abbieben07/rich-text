4c05cbab55d02d42f9f36333c58835ddb976d13f
Abbieben <abraham.benobaje@versecom.com.ng>
2021-10-28 13:25:13 +0100

    textChange Fixes

---
+++ src/phone.common.ts
@@ -0,0 +1,23 @@
+import { AsYouType, CountryCode, getCountryCallingCode } from 'libphonenumber-js'
+
+//@ts-ignore
+String.prototype.decode = function () {
+    const map = { gt: '>' /* , … */ };
+    return this.replace(/&(#(?:x[0-9a-f]+|\d+)|[a-z]+);?/gi, ($0, $1) => {
+        if ($1[0] === '#') {
+            return String.fromCharCode($1[1].toLowerCase() === 'x' ? parseInt($1.substr(2), 16) : parseInt($1.substr(1), 10));
+        } else {
+            return map.hasOwnProperty($1) ? map[$1] : $0;
+        }
+    });
+};
+
+export function FormatPhone(phone: string, country: CountryCode): string {
+    const formatter = new AsYouType(country);
+    const code = `+${getCountryCallingCode(country)}`;
+    if (phone.match())
+        return;
+    const result = formatter.input(phone);
+    console.log("before", phone, "after", result)
+    return result;
+}
